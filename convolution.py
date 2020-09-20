79aeaf23d716acb06b729bf81d29db555972a606
penrin <hrs@penr.in>
2020-09-20 18:05:04 +0900

    changed the class structure of convolution

--- convolution.py
+++ convolution.py
@@ -1,244 +1,264 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+dtype_float = {'float': np.float32, 'double':np.float64}
+dtype_complex = {'float': np.complex64, 'double':np.complex128}
 
 
 class OverlapSave:
-    # N-point overlap-save.
-    # convolve SISO FIR to all channels in parallel.
-    # fir.ndim shoud be 1.
-    # schedule: step of delay (schedule * N taps)
-    
-    def __init__(self, fir, N, channel=1, schedule=0, dtype=np.float32):
-    
+    # N-point overlap-save convolution
+    # 
+    # SISO FIR is applied in parallel to all channels.
+    # fir.ndim should be 1.
+    # FIR longer than N is automatically chunked.
+
+    def __init__(self, fir, N, channel, dtype='float'):
         if fir.ndim != 1:
             raise Exception('invalid fir shape')
 
         self.N = N
-        self.len_fir = len(fir)
-        fir_zeropad = np.zeros(2 * N, dtype=dtype)
-        fir_zeropad[N:N + self.len_fir] = fir
+        self.len_fir = fir.shape[-1]
 
-        self.fir_f = np.fft.rfft(fir_zeropad)
-        self.x = np.zeros([channel, 2 * N], dtype=dtype)
-        self.y_buff = np.zeros([schedule + 1, channel, N], dtype=dtype)
-        self.i_buff = 0
-
-        self.len_y = fir.shape[-1]
-        self.len_y_buff = np.empty([schedule + 1], dtype=np.int)
-        self.len_y_buff[:] = N
+        # split FIR
+        fir_split = []
+        ss = 0
+        while ss < fir.shape[-1]:
+            fir_split.append(fir[ss:ss + N])
+            ss += N
         
+        # instantiate convolver
+        self.c = []
+        for i in range(len(fir_split)):
+            self.c.append(_Convolver(fir_split[i], N, channel, i, dtype))
+        
+        # input buffer
+        dt_r = dtype_float[dtype]
+        self.in_buf = np.zeros([channel, 2 * N], dtype=dt_r)
+        
+        # output
+        dt_c = dtype_complex[dtype]
+        self.out_f = np.zeros([channel, N + 1], dtype=dt_c)
+
+        # buffering length
+        self.len_buf = self.N
+        
+    def conv(self, x):
+
+        # shifted into buffer
+        len_in = x.shape[-1]
+        self.in_buf[:, :self.N] = self.in_buf[:, self.N:]
+        self.in_buf[:, self.N:self.N + len_in] = x
+        self.in_buf[:, self.N + len_in:] = 0
+
+        # convolution
+        in_f = np.fft.rfft(self.in_buf)
+        self.out_f[:] = 0
+        for c_ in self.c:
+            self.out_f[:] += c_.conv(in_f)
+        out = np.fft.irfft(self.out_f)[:, :self.N]
+
+        # buffering length
+        if len_in != 0:
+            self.len_buf = self.len_fir + len_in - 1
+        else:
+            self.len_buf -= self.N
+
+        return out, self.len_buf
+            
+    
+    def clear_buffer(self):
+        for c_ in self.c:
+            c_.out_f_buf[:] = 0
+
+
+class _Convolver:
+
+    def __init__(self, fir, N, channel, schedule, dtype):
+        self.N = N
         self.schedule = schedule
 
-
-    def conv(self, x):
+        # FIR (frequency domain)
+        dt_r = dtype_float[dtype]
+        fir_zeropad = np.zeros(2 * N, dtype=dt_r)
+        fir_zeropad[N:N + fir.shape[-1]] = fir
+        self.fir_f = np.fft.rfft(fir_zeropad)
         
-        # shifted into buffer
-        len_x = x.shape[-1]
-        self.x[:, :self.N] = self.x[:, self.N:]
-        self.x[:, self.N:self.N + len_x] = x
-        self.x[:, self.N + len_x:] = 0
+        # output buffer delay output timing by N * schedule
+        dt_c = dtype_complex[dtype]
+        self.out_f_buf = np.zeros([schedule + 1, channel, N + 1], dtype=dt_c)
+        self.i_buf = 0
         
+    def conv(self, x_f):
         # convolution
-        X = np.fft.rfft(self.x)
-        self.y_buff[self.i_buff] = np.fft.irfft(self.fir_f * X)[:, :self.N]
+        self.out_f_buf[self.i_buf] = self.fir_f * x_f
+        # index ring buffer
+        self.i_buf += 1
+        if self.i_buf > self.schedule:
+            self.i_buf = 0
+        return self.out_f_buf[self.i_buf]
 
-        # length of output
-        if len_x != 0:
-            self.len_y = len_x + self.len_fir - 1
-            
-        elif self.len_y:
-            self.len_y -= self.N
-            if self.len_y < 0:
-                self.len_y = 0
 
-        if self.len_y > self.N:
-            self.len_y_buff[self.i_buff] = self.N
-        else:
-            self.len_y_buff[self.i_buff] = self.len_y
 
-        # ring buffer for delay of y
-        self.i_buff += 1
-        if self.i_buff > self.schedule:
-            self.i_buff = 0
-                
-        return self.y_buff[self.i_buff], self.len_y_buff[self.i_buff]
-    
-
-    def clear(self):
-        self.x[:] = 0
-        self.y_buff[:] = 0
 
 
 
 class OverlapSaveMIMO:
-    # N-point overlap-save.
-    # convolve MIMO FIR to all channels in parallel.
-    # fir.ndim shoud be 3.
-    # schedule: step of delay (schedule * N taps)
-    
-    def __init__(self, fir, N, schedule=0, dtype=np.float32):
+    # N-point overlap-save convolution
+    # 
+    # MIMO FIR is applied.
+    # fir.ndim should be 1.
+    # FIR longer than N is automatically chunked.
 
+    def __init__(self, fir, N, dtype=np.float32):
         if fir.ndim != 3:
             raise Exception('invalid fir shape')
-        
-        ch_out = fir.shape[0]
-        ch_in = fir.shape[1]
-        self.len_fir = fir.shape[2]
 
         self.N = N
-        fir_zeropad = np.zeros([ch_out, ch_in, 2 * N], dtype=dtype)
-        fir_zeropad[:, :, N:N + self.len_fir] = fir
+        self.len_fir = fir.shape[-1]
 
-        self.fir_f = np.fft.rfft(fir_zeropad).transpose(2, 0, 1)
-        self.x = np.zeros([ch_in, 1, 2 * N], dtype=dtype)
-        self.y_buff = np.zeros([schedule + 1, ch_out, N], dtype=dtype)
-        self.i_buff = 0
+        ch_out = fir.shape[0]
+        ch_in = fir.shape[1]
 
-        self.len_y = fir.shape[-1]
-        self.len_y_buff = np.empty([schedule + 1], dtype=np.int)
-        self.len_y_buff[:] = N
+        # split FIR
+        fir_split = []
+        ss = 0
+        while ss < fir.shape[-1]:
+            fir_split.append(fir[:, :, ss:ss + N])
+            ss += N
         
-        self.schedule = schedule
+        # instantiate convolver
+        self.c = []
+        for i in range(len(fir_split)):
+            self.c.append(_ConvolverMIMO(fir_split[i], N, i, dtype))
+        
+        # input buffer
+        dt_r = dtype_float[dtype]
+        self.in_buf = np.zeros([ch_in, 1, 2 * N], dtype=dt_r)
+        
+        # output
+        dt_c = dtype_complex[dtype]
+        self.out_f = np.zeros([ch_out, N + 1], dtype=dt_c)
+
+        # buffering length
+        self.len_buf = self.N
 
 
     def conv(self, x):
-        
+
         # shifted into buffer
-        len_x = x.shape[-1]
-        self.x[:, 0, :self.N] = self.x[:, 0, self.N:]
-        self.x[:, 0, self.N:self.N + len_x] = x
-        self.x[:, 0, self.N + len_x:] = 0
-        
+        len_in = x.shape[-1]
+        self.in_buf[:, 0, :self.N] = self.in_buf[:, 0, self.N:]
+        self.in_buf[:, 0, self.N:self.N + len_in] = x
+        self.in_buf[:, 0, self.N + len_in:] = 0
+
         # convolution
-        x_f = np.fft.rfft(self.x).transpose(2, 0, 1)
-        y_f = np.matmul(self.fir_f, x_f).transpose(1, 2, 0)
-        self.y_buff[self.i_buff] = np.fft.irfft(y_f)[:, 0, :self.N]
+        in_f = np.fft.rfft(self.in_buf).transpose(2, 0, 1)
+        self.out_f[:] = 0
+        for c_ in self.c:
+            self.out_f[:] += c_.conv(in_f)
+        out = np.fft.irfft(self.out_f)[:, :self.N]
 
-        # length of output
-        if len_x != 0:
-            self.len_y = len_x + self.len_fir - 1
-            
-        elif self.len_y:
-            self.len_y -= self.N
-            if self.len_y < 0:
-                self.len_y = 0
-
-        if self.len_y > self.N:
-            self.len_y_buff[self.i_buff] = self.N
+        # buffering length
+        if len_in != 0:
+            self.len_buf = self.len_fir + len_in - 1
         else:
-            self.len_y_buff[self.i_buff] = self.len_y
+            self.len_buf -= self.N
 
-        # ring buffer for delay of y
-        self.i_buff += 1
-        if self.i_buff > self.schedule:
-            self.i_buff = 0
-                
-        return self.y_buff[self.i_buff], self.len_y_buff[self.i_buff]
+        return out, self.len_buf
+            
     
+    def clear_buffer(self):
+        for c_ in self.c:
+            c_.out_buf[:] = 0
 
-    def clear(self):
-        self.x[:] = 0
-        self.y_buff[:] = 0
+
+class _ConvolverMIMO:
+
+    def __init__(self, fir, N, schedule, dtype):
+        self.N = N
+        self.schedule = schedule
+
+        ch_out = fir.shape[0]
+        ch_in = fir.shape[1]
+        
+        # FIR (frequency domain)
+        dt_r = dtype_float[dtype]
+        fir_zeropad = np.zeros([ch_out, ch_in, 2 * N], dtype=dt_r)
+        fir_zeropad[:, :, N:N + fir.shape[-1]] = fir
+        self.fir_f = np.fft.rfft(fir_zeropad).transpose(2, 0, 1)
+        
+        # output buffer delay output timing by N * schedule
+        dt_c = dtype_complex[dtype]
+        self.out_f_buf = np.zeros([schedule + 1, ch_out, N + 1], dtype=dt_c)
+        self.i_buf = 0
+        
+    def conv(self, x_f):
+        # convolution
+        y_f = np.matmul(self.fir_f, x_f).transpose(1, 2, 0)
+        self.out_f_buf[self.i_buf] = y_f[:, 0, :]
+        # index ring buffer
+        self.i_buf += 1
+        if self.i_buf > self.schedule:
+            self.i_buf = 0
+        return self.out_f_buf[self.i_buf]
+
 
 
 
 
 if __name__ == '__main__':
-
+    
     import matplotlib.pyplot as plt
+    
     
     # ----- convolve using OverlapSave() -----
 
-    # input signal and FIR
-    src = np.ones(100).reshape(1, -1)
-    fir = np.ones(40)
+    x = np.ones(80).reshape(1, -1)
+    h = np.ones(40)
     N = 32
-    
-    # split FIR
-    n_ch = src.shape[0]
-    len_fir = len(fir)
-    len_out = src.shape[-1] + len_fir - 1
-    n_split = int(np.ceil(len_fir / N))
-    fir_n = []
-    for i in range(n_split):
-        ss = N * i
-        to = ss + N
-        if to > len_fir:
-            to = len_fir
-        
-        os = OverlapSave(fir[ss:to], N, channel=n_ch, schedule=i)
-        fir_n.append(os)
-        print(i, fir[:, :, ss:to].shape)
-    
-    # convolution
-    out = np.empty([n_ch, 0])
-    out_n = np.empty([n_ch, N])
+    dtype = 'float'
 
+    os = OverlapSave(h, N, channel=x.shape[0], dtype=dtype)
+    
+    len_y = h.shape[-1] + x.shape[-1] - 1
+    y = np.empty([x.shape[0], len_y], dtype=dtype)
     ss = 0
-    to = ss + N
     while True:
-        out_n[:] = 0
-        for n in range(n_split):
-            out_n_, L = fir_n[n].conv(src[:, ss:to])
-            out_n += out_n_
-        out = np.c_[out, out_n[:, :L]]
+        y_chunk, len_buf = os.conv(x[:, ss:ss + N])
+        L = np.clip(len_buf, 0, N)
+        y[:, ss:ss + L] = y_chunk[:, :L]
 
         if L < N:
             break
         ss += N
-        to += N
-    
-    print(out.shape)
-    for i in range(out.shape[0]):
-        plt.subplot(out.shape[0], 1, i + 1)
-        plt.plot(out[i, :], lw=1)
+         
+    plt.plot(y[0], lw=1)
     plt.show()
-    
     
 
     # ----- convolve using OverlapSaveMIMO() -----
-
-    # input signal and FIR
-    src = np.ones(100).reshape(1, -1)
-    fir = np.zeros([2, 1, 40]) # 1in/2out
+    
+    x = np.ones(160).reshape(2, -1)
+    h = np.ones([2, 2, 40]) # 2in/2out
     N = 32
-
-    # split FIR
-    n_ch = src.shape[0]
-    len_fir = fir.shape[-1]
-    len_out = src.shape[-1] + len_fir - 1
-    n_split = int(np.ceil(len_fir / N))
-    fir_n = []
-    for i in range(n_split):
-        ss = N * i
-        to = ss + N
-        if to > len_fir:
-            to = len_fir
-
-        os = OverlapSaveMIMO(fir[:, :, ss:to], N, schedule=i)
-        fir_n.append(os)
-        print(i, fir[:, :, ss:to].shape)
-
-    # convolution
-    out = np.empty([fir.shape[0], 0])
-    out_n = np.empty([fir.shape[0], N])
-
+    dtype = 'float'
+    
+    os = OverlapSaveMIMO(h, N, dtype=dtype)
+    
+    len_y = h.shape[-1] + x.shape[-1] - 1
+    y = np.empty([h.shape[0], len_y], dtype=dtype)
     ss = 0
-    to = ss + N
     while True:
-        out_n[:] = 0
-        for n in range(n_split):
-            out_n_, L = fir_n[n].conv(src[:, ss:to])
-            out_n += out_n_
-        out = np.c_[out, out_n[:, :L]]
+        y_chunk, len_buf = os.conv(x[:, ss:ss + N])
+        L = np.clip(len_buf, 0, N)
+        y[:, ss:ss + L] = y_chunk[:, :L]
 
         if L < N:
             break
         ss += N
-        to += N
-
-    print(out.shape)
-    for i in range(out.shape[0]):
-        plt.subplot(out.shape[0], 1, i + 1)
-        plt.plot(out[i, :], lw=1)
-    plt.show()
     
+    plt.plot(y[0], lw=1)
+    plt.show()
+
+
